2a29212359559a1d004c4baca7ee7cb2f5d78531
Mauricio Teruel <mauricioteruel98@gmail.com>
2024-03-19 17:06:49 -0300

    changes

--- src/components/RedesSocialesInicio.jsx
+++ src/components/RedesSocialesInicio.jsx
@@ -71,7 +71,7 @@ const RedesSocialesInicio = () => {
                                 strokeLinejoin="round"
                                 strokeWidth="2"
                                 d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                        <span>Email copiado en portapapeles</span>
+                        <span>Dirección de E-mail copiado en portapapeles</span>
                     </div>
                 </div>
             )}
